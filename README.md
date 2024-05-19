39e97fd3d9d22ca5978052eea0fec8e8ac6caf84
Ashish Baruah <ashishbaruah90@gmail.com>
2024-05-19 16:07:59 +0530

    Data_ingestion Pipeline Created

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Xray-for-Pneumonia
+
+## Workflows
+
+1. Update config.yamml done
+2. Update secrets.yaml 
+3. params.yaml  done
+4. update the entity    done
+5. update the configuration manager in src config   done
+6. update the components    done
+7. Update the pipeline  done
+8. update the main.py   done    
+9. update the dvc.yaml
+10. app.py
+
+Data ingestion components
--- main.py
+++ main.py
@@ -1,3 +1,37 @@
-from src.xray import logger
+# from src.xray import logger
+# from xray import logger
+# from xray.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to custom log")
+# ---------------------------------------------------------
+
+import os
+from xray import logger
+from xray.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+# from xray.pipeline.training_pipeline import TrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>>> Stage {STAGE_NAME} Started <<<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>> Stage {STAGE_NAME} Completed <<<<<<<\n\nx====================x")
+     
+except Exception as e:
+    logger.exception(e)
+    raise(e)
+ 
+# STAGE_NAME = "Training Stage"
+
+# if __name__ == "__main__":
+#     try:
+#         logger.info(f"************************")
+#         logger.info(f">>>>>>> Stage {STAGE_NAME} Started <<<<<<<")
+#         obj = TrainingPipeline()
+#         obj.main()
+#         logger.info(f">>>>>>> Stage {STAGE_NAME} Completed <<<<<<<\n\nx====================x")
+        
+#     except Exception as e:
+#         logger.exception(e)
+#         raise(e)
